6c378cc6334dc11cf1286888dc5ac6b8e47d0c47
LeidyAyalaSosa <ayalaleidy40@gmail.com>
2022-07-10 22:16:16 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TitleComponent from "./components/TitleComponent";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <TitleComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,20 @@
+import "../styles/AppStyles.css";
+
+function TitleComponent() {
+  return (
+    <div className="Container">
+      <h1>MI PRIMERA APP</h1>
+      <label for="id-input" className="from-label">
+        Id:
+      </label>
+      <input type="number" id="id-input" className="from-input" />
+      <label for="name-input" className="from-label">
+        Name:
+      </label>
+      <input type="text" id="name-input" className="from-input" />
+      <button>ENVIAR</button>
+    </div>
+  );
+}
+
+export default TitleComponent;
